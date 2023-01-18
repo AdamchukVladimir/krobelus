0ee5513e11a5a4764d434dd1b2efc57e0d99d01b
AdamchukVladimir <vldmr.adamchuk@gmail.com>
2023-01-18 23:02:12 +0300

    add imgs

---
+++ src/services/HeroListTest.vue
@@ -0,0 +1,41 @@
+<script>
+/*
+<template>
+  <div class="heroes">
+    <div class="hero str">
+      <template v-for="hero in result.constants.heroes" :key="hero.id">
+        <div class="heroPick" v-if="hero.stats.primaryAttribute == 'str'">
+          <HeroAvatar :heroId="hero.id" />
+        </div>
+      </template>
+    </div>
+    <div class="hero agi">
+      <template v-for="hero in result.constants.heroes" :key="hero.id">
+        <div
+          class="heroPick"
+          v-for="hero in result.constants.heroes"
+          :key="hero.id"
+        >
+          <HeroAvatar
+            v-if="hero.stats.primaryAttribute == 'agi'"
+            :heroId="hero.id"
+          />
+        </div>
+      </template>
+    </div>
+    <div class="hero int">
+      <div
+        class="heroPick"
+        v-for="hero in result.constants.heroes"
+        :key="hero.id"
+      >
+        <HeroAvatar
+          v-if="hero.stats.primaryAttribute == 'int'"
+          :heroId="hero.id"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+*/
+</script>
--- src/views/HeroList.vue
+++ src/views/HeroList.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="heroes">
-    <div
-      class="hero heroPick"
-      v-for="hero in result.heroStats.stats"
-      :key="hero.heroId"
-    >
-      <HeroAvatar :heroId="hero.heroId" />
+  <div class="heroes heroesPull">
+    <div class="hero str">
+      <template v-for="hero in result.constants.heroes" :key="hero.id">
+        <div class="heroPick" v-if="hero.stats.primaryAttribute == 'str'">
+          <HeroAvatar :heroId="hero.id" />
+        </div>
+      </template>
+    </div>
+    <div class="hero agi">
+      <template v-for="hero in result.constants.heroes" :key="hero.id">
+        <div class="heroPick" v-if="hero.stats.primaryAttribute == 'agi'">
+          <HeroAvatar :heroId="hero.id" />
+        </div>
+      </template>
+    </div>
+    <div class="hero int">
+      <template v-for="hero in result.constants.heroes" :key="hero.id">
+        <div class="heroPick" v-if="hero.stats.primaryAttribute == 'int'">
+          <HeroAvatar :heroId="hero.id" />
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -17,14 +31,41 @@ import HeroAvatar from "@/components/HeroAvatar.vue";
 
 const CHARACTERS_QUERY = gql`
   {
+    constants {
+      heroes {
+        id
+        stats {
+          primaryAttribute
+        }
+      }
+    }
+  }
+`;
+/*
+<template>
+  <div class="heroes">
+    <div
+      class="hero heroPick"
+      v-for="hero in result.constants.heroes"
+      :key="hero.id"
+    >
+      <div :class="hero.stats.primaryAttribute">
+        <HeroAvatar :heroId="hero.id" />
+      </div>
+    </div>
+  </div>
+</template>
+
+      <p>{{ hero.stats.primaryAttribute }}</p>
+
+  {
     heroStats {
       stats {
         heroId
       }
     }
   }
-`;
-/*
+
   <HeroAvatar v-for="hero in heroes" :key="hero.id" />
 
     <p v-if="error">{{ error }}</p>
@@ -83,5 +124,32 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.heroes {
+  padding-left: auto;
+  padding-right: auto;
+}
+.str {
+  display: grid;
+  width: auto;
+
+  gap: 5px;
+  grid-template-columns: 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px;
+}
+.agi {
+  display: grid;
+  width: auto;
+  margin-left: auto;
+  margin-right: auto;
+  gap: 5px;
+  grid-template-columns: 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px;
+}
+.int {
+  display: grid;
+  width: auto;
+  margin-left: auto;
+  margin-right: auto;
+  gap: 5px;
+  grid-template-columns: 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px;
 }
 </style>
