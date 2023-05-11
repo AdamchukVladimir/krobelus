b34509470b8de1e2346639519c78e65f335fd6f4
AdamchukVladimir <vldmr.adamchuk@gmail.com>
2023-05-12 00:28:05 +0300

    add BestHeroes to dashboard

--- src/components/BestHeroes.vue
+++ src/components/BestHeroes.vue
@@ -2,14 +2,25 @@
   <Header />
   <div class="wrapper_profile">
     <div class="profile_avatar"><img :src="fAvatarImage" /></div>
-    <div class="heroes_bestHeoes">
+    <div class="heroes_summary">
       <div class="simpleHeroes">
         <template
           v-for="hero in fSimpleSummary.Simpleheroes"
-          :key="hero.heroId + hero.winCount"
+          :key="hero.id + hero.winCount"
         >
           <div class="simpleHero" v-if="hero.id > 0">
             <HeroAvatar :heroObj="hero" />
+          </div>
+        </template>
+      </div>
+      <div class="bestHeroes">
+        <template
+          v-for="hero in fBestheroes.BestHeroes"
+          :key="hero.heroId + hero.winCount"
+        >
+          <div class="bestHero" v-if="hero.activity == true">
+            <HeroAvatar :heroObj="hero" />
+            {{ hero.winCount }}
           </div>
         </template>
       </div>
@@ -46,8 +57,48 @@ export default {
       userHeroesStore: "userHeroes",
     }),
     fAvatarImage() {
-      console.log("userSteamAvatarStore " + this.userSteamAvatarStore);
       if (this.userSteamAvatarStore) return this.userSteamAvatarStore;
+    },
+    fBestheroes() {
+      let oBestheroes = {
+        BestHeroes: [
+          {
+            id: 0,
+            imgId: 0,
+            winCount: 0,
+            lossCount: 0,
+            lastPlayedDateTime: 0,
+            activity: false,
+          },
+        ],
+      };
+      if (this.userHeroesStore.player.heroesPerformance) {
+        for (
+          let i = 0;
+          i < this.userHeroesStore.player.heroesPerformance.length;
+          i++
+        ) {
+          oBestheroes.BestHeroes.push({
+            id: this.userHeroesStore.player.heroesPerformance[i].heroId,
+            imgId: this.userHeroesStore.player.heroesPerformance[i].heroId,
+            winCount: this.userHeroesStore.player.heroesPerformance[i].winCount,
+            lossCount:
+              this.userHeroesStore.player.heroesPerformance[i].lossCount,
+            lastPlayedDateTime:
+              this.userHeroesStore.player.heroesPerformance[i]
+                .lastPlayedDateTime,
+            activity:
+              parseInt(new Date().getTime() / 1000) -
+                this.userHeroesStore.player.heroesPerformance[i]
+                  .lastPlayedDateTime <
+              5259486,
+          });
+        }
+        oBestheroes.BestHeroes.sort((a, b) =>
+          a.winCount > b.winCount ? 1 : -1
+        );
+      }
+      return oBestheroes;
     },
     fSimpleSummary() {
       let oSimpleSummary = {
@@ -65,7 +116,6 @@ export default {
           i < this.userHeroesStore.player.simpleSummary.heroes.length;
           i++
         ) {
-          console.log("fSimpleSummary ");
           oSimpleSummary.Simpleheroes.push({
             id: this.userHeroesStore.player.simpleSummary.heroes[i].heroId,
             imgId: this.userHeroesStore.player.simpleSummary.heroes[i].heroId,
@@ -84,7 +134,7 @@ export default {
     console.log("mounteduserSteamAvatarStore " + this.userSteamAvatarStore);
     console.log(
       "userHeroesVue " +
-        JSON.stringify(this.userHeroesStore.player.simpleSummary.heroes)
+        JSON.stringify(this.userHeroesStore.player.heroesPerformance)
     );
   },
 };
@@ -98,7 +148,30 @@ export default {
   grid-gap: 50px 10px;
   grid-template-columns: 3fr 4fr;
   grid-template-areas:
-    "profile_avatar heroes_bestHeoes"
+    "profile_avatar heroes_summary"
     "settings recomendationHeroes";
+}
+
+.heroes_summary {
+  grid-area: heroes_summary;
+  display: grid;
+  width: 500px;
+  margin-bottom: 10px;
+
+  grid-template-columns: 240px;
+}
+
+.simpleHeroes {
+  display: grid;
+  width: 150px;
+  gap: 7px;
+  grid-template-columns: 50px 50px 50px;
+}
+
+.bestHeroes {
+  display: grid;
+  width: auto;
+  gap: 7px;
+  grid-template-columns: 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px 50px;
 }
 </style>
